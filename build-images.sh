a19e1da20556342956a909a7111d7f2e81e63ffb
Kemboi Elvis <kemboielvis22@gmail.com>
2025-07-23 12:11:39 +0300

    refactor: replaced all kickstart names with windmill

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="windmill"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-windmill container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-windmill; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-windmill -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-windmill \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
